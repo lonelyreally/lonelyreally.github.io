5bedf3afbd49cf920bdd8abbd16290fb80dbdba6
lonelyreally <yarikzaladovsky@gmail.com>
2025-10-10 03:12:00 +0500

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,30 @@
+function checkForm1(el1) {
+
+    var answer1 = el1.ANSWER1.value;
+    if (answer1 == 0.9) {
+        alert('Верно!')
+    } else {
+        alert('Неправильно! Наталья Петровна будет тобой недовольна...')
+    }
+    return false
+}
+function checkForm2(el2) {
+
+    var answer2 = el2.ANSWER2.value;
+    if (answer2 == 174) {
+        alert('Верно!')
+    } else {
+        alert('Неправильно! Наталья Петровна будет тобой недовольна...')
+    }
+    return false
+}
+function checkForm3(el3) {
+
+    var answer3 = el3.ANSWER3.value;
+    if (answer3 == 98) {
+        alert('Верно!')
+    } else {
+        alert('Неправильно! Наталья Петровна будет тобой недовольна...')
+    }
+    return false
+}
